cfaabbf8766754a129934089d080525a273c57f3
ganesh-bhumarapu <ganesh.bhumarapu@gmail.com>
2024-08-16 11:53:41 +0530

    Modified File

---
+++ TopologicalSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class TopologicalSort{
+    public static void dfs(int node, int[] vis, ArrayList<ArrayList<Integer>> adj, Stack<Integer> st){
+        vis[node] = 1;
+        for(int it : adj.get(node)){
+            if(vis[node] == 0){
+                dfs(node, vis, adj, st);
+            }
+        }
+        st.push(node);
+    }
+    public static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj){
+        int[] vis = new int[V];
+        Stack<Integer> st = new Stack();
+        for(int i = 0; i < V; i++){
+            if(vis[i] == 0){
+                dfs(i, vis, adj, st);
+            }
+        }
+        int[] res = new int[V];
+        int i = 0;
+        while(!st.isEmpty()){
+            res[i++] = st.peek();
+            st.pop();
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int V = sc.nextInt();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<>>();
+        for(int i = 0; i< V; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < V; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            adj.get(u).add(v);
+        }
+        int[] res = topoSort(V, adj);
+        for(int i : res){
+            System.out.print(i);
+        }
+    }
+}
